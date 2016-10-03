0608481140dd3b8dac2bcbc5d0172ece01a5d0bc
alumno <alumno@328-13>
2016-10-03 10:53:52 -0500

    FP-Menu muestra de menus consola en java

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package fp.menu;
+import java.util.*;
 
 /**
  *
@@ -15,7 +16,20 @@ public class FPMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("La opcion elejida es:"+menu());
+    }
+    public static int menu(){
+        int opcion;
+        Scanner entrada=new Scanner (System.in);
+        System.out.println("***Calculo de área***");
+        System.out.println("***Menú***");
+        System.out.println("Introduce la opcion deseada");
+        System.out.println("1. Area de cuadrados");
+        System.out.println("2. Area de circulos");
+        System.out.println("3. Area de triangulos");
+        opcion=entrada.nextInt();
+        return opcion;
+    
     }
     
 }
